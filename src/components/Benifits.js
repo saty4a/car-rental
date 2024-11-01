c20e97b88b372af3bf9a892cc0bf62cee410906b
satyabrata biswal <86743992+saty4a@users.noreply.github.com>
2024-11-01 11:52:20 +0530

    Update Benifits.js

--- src/components/Benifits.js
+++ src/components/Benifits.js
@@ -26,7 +26,7 @@ const BenifitSection = () => {
 
     return(
         <div className="flex flex-col lg:flex-row mx-5 lg:mx-0 justify-between my-5">
-            {benifitData && benifitData.map((data) => (<div className="flex flex-col items-center gap-5">
+            {benifitData && benifitData.map((data, index) => (<div key={index} className="flex flex-col items-center gap-5">
                     <Image src={data.image} alt={data.altText} width={50} height={50} />
                     <h3 className="font-bold text-xl">{data.label}</h3>
                     <p className="text-center">{data.desc}</p>
